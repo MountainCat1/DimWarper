2208447c70731e6fb700dc64f739bd22e20a331b
mariuszgajewski13 <mariusz.gajewski@microsoft.wsei.edu.pl>
2022-01-28 19:20:45 +0100

    Menu

---
+++ Duality/Assets/Scripts/Scenes/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Launching game...");
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+    
+
+}
