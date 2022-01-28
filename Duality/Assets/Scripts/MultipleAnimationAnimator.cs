6c9f07487ffd7a18d019aaa4cbbee21e6d52e44c
MountainCat1 <72751263+MountainCat1@users.noreply.github.com>
2022-01-28 01:54:23 +0100

    added MultipleAnimationAnimator

---
+++ Duality/Assets/Scripts/MultipleAnimationAnimator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MultipleAnimationAnimator : SpriteAnimator
+{
+    [SerializeField] private List<string> framesLocations = new List<string>();
+    private bool animationsLoaded = false;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if (!animationsLoaded)
+            LoadAnimations();
+    }
+
+    private void LoadAnimations()
+    {
+        List<SpriteAnimation> loadedAnimations = new List<SpriteAnimation>();
+
+        string basePath = $"Sprites/Animations";
+
+        foreach (var animationPath in framesLocations)
+        {
+            loadedAnimations.Add(new SpriteAnimation()
+            {
+                Frames = Resources.LoadAll<Sprite>($"{basePath}/{animationPath}").ToArray(),
+                Name = "animationPath"
+            });
+        }
+
+        animations = loadedAnimations;
+        animationsLoaded = true;
+    }
+}
