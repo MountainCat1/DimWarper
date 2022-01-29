bfe2e1039db0c83a1979e22cdabe65c7e13538ef
MountainCat1 <72751263+MountainCat1@users.noreply.github.com>
2022-01-29 18:48:18 +0100

    Changed scene loading to async

--- Duality/Assets/GameOverManager.cs
+++ Duality/Assets/GameOverManager.cs
@@ -19,11 +19,29 @@ public class GameOverManager : MonoBehaviour
 
     public void Restart()
     {
-        SceneManager.LoadScene("Game");
+        StartCoroutine(AsyncLoadScene("Game"));
     }
 
     public void LoadMenu()
     {
-        SceneManager.LoadScene("MainMenu");
+        StartCoroutine(AsyncLoadScene("MainMenu"));
+    }
+
+    IEnumerator AsyncLoadScene(string sceneName)
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        Debug.Log("=== Game loaded... ===");
     }
 }
--- Duality/Assets/Scripts/Scenes/MainMenu.cs
+++ Duality/Assets/Scripts/Scenes/MainMenu.cs
@@ -1,12 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        Debug.Log("Launching game...");
-        SceneManager.LoadScene("Game");
+        Debug.Log("=== Loading game... ===");
+
+        StartCoroutine(LoadYourAsyncScene());
     }
 
     public void Quit()
@@ -14,6 +16,23 @@ public class MainMenu : MonoBehaviour
         Debug.Log("Quit!");
         Application.Quit();
     }
-    
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Game");
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        Debug.Log("=== Game loaded... ===");
+    }
 
 }
