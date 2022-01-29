4f501e156b9fbb7a15c34a2f1de7b72c2045d969
mariuszgajewski13 <mariusz.gajewski@microsoft.wsei.edu.pl>
2022-01-29 19:26:59 +0100

    cutscene transition

--- Duality/Assets/Lore.cs
+++ Duality/Assets/Lore.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Lore : MonoBehaviour
 {
     string lore = "What … what happened??? – you ask. You wake up alone in mysterious tower, in front of you there is a note and a strange amulet. The note:You have been chosen for my challenge. Complete it and you will become more powerful than you could ever imagine, fail and loose you sanity. Your goal is to get on top of my tower, but many challenges lay in front of you.The amulet I gave you allows you to change physical properties of the tower, which will help you complete the quest. Also, you can’t die. XOXO Evil Magician.";
 
     public float writeSpeed = 0.05f;
+    public float waitingTime = 5f;
 
     public AudioSource beep;
+    public Animator fade;
 
     void Start()
     {
@@ -24,6 +26,9 @@ public class Lore : MonoBehaviour
             GetComponent<Text>().text += lore[i];
             yield return new WaitForSeconds(writeSpeed);
         }
+        yield return new WaitForSeconds(waitingTime);
+        fade.SetBool("fade", true);
+        SceneManager.LoadScene("Game");
     }
 
 
