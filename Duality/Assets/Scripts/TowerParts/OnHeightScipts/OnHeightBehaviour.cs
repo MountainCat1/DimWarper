057e8ed22992fcb097a9781f22d2f71a020696bb
MountainCat1 <tomasz.banas123@gmail.com>
2022-04-28 14:16:24 +0200

    Small fixes and not that important changes

---
+++ Duality/Assets/Scripts/TowerParts/OnHeightScipts/OnHeightBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class OnHeightBehaviour : MonoBehaviour
+{
+    public float height;
+
+    private bool fired = false;
+
+    private void Update()
+    {
+        if(!fired && GameManager.Instance.ExpectedHeight >= height)
+        {
+            fired = true;
+            Debug.Log($"Fired OnHeightBehaviour {gameObject.name} {this.GetType()}");
+            Action();
+        }
+    }
+
+    protected abstract void Action();
+}
