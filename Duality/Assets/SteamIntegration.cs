2d6c2e84feddce851e0ce2e2785cc6d4c561347d
Tomasz Banaś <72751263+MountainCat1@users.noreply.github.com>
2022-06-19 17:27:47 +0200

    Added steam integration

---
+++ Duality/Assets/SteamIntegration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class SteamIntegration : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        try
+        {
+            Steamworks.SteamClient.Init(2056190);
+            Debug.Log($"Logged to steam as {Steamworks.SteamClient.Name}");
+        }
+        catch(Exception ex)
+        {
+            Debug.LogError($"Error accrued while trying to connect to to steam!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
