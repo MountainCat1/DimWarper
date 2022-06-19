209843d772b18cd13455d3d13f46de3b1a74076d
Tomasz Banaś <72751263+MountainCat1@users.noreply.github.com>
2022-06-19 23:24:07 +0200

    Added achievements for progress

---
+++ Duality/Assets/Scripts/SteamIntegration/SteamAchievementForProgress.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Steamworks;
+using UnityEngine;
+
+public class SteamAchievementForProgress : MonoBehaviour
+{
+    public List<SteamAchievementForProgressModel> Achievements = new List<SteamAchievementForProgressModel>();
+
+    private void Start()
+    {
+        Debug.Log("Checking for steam achievements for game progress...");
+
+        var data = GameDataManager.Data;
+        
+        foreach (var achievement in Achievements)
+        {
+            if (achievement.requiredGameProgress >= data.gameProgress)
+            {
+                Debug.Log($"Achievement unlocked! {achievement.steamAchievementId}");
+
+                UnlockSteamAchievement(achievement.steamAchievementId);
+            }
+        }
+    }
+
+
+    private void UnlockSteamAchievement(string id)
+    {
+        var steamAchievement = new Steamworks.Data.Achievement(id);
+        steamAchievement.Trigger();
+    }
+}
+
+[System.Serializable]
+public struct SteamAchievementForProgressModel
+{
+    public string steamAchievementId;
+    public int requiredGameProgress;
+}
---
+++ Duality/Assets/Scripts/SteamIntegration/SteamIntegrationInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Steamworks;
+using UnityEngine;
+
+public class SteamIntegrationInitializer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        try
+        {
+            if (SteamClient.IsLoggedOn)
+            {
+                Debug.Log($"<Steam Initialized> Already logged in as {SteamClient.Name}");
+                return;
+            }
+        }
+        catch (Exception e)
+        {
+            // ignored
+        }
+
+
+        try
+        {
+            SteamClient.Init(2056190);
+            Debug.Log($"<Steam Initialized> Logged to steam as {SteamClient.Name}");
+        }
+        catch(Exception ex)
+        {
+            Debug.LogError($"<Steam Initialized> Error accrued while trying to connect to to steam!");
+        }
+    }
+}
